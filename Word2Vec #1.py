24c295fbca8b43b8fd0ffaa18147eef88835df5a
yinanericxue <102645083+yinanericxue@users.noreply.github.com>
2022-07-21 01:47:17 -0700

    Update Word2Vec #1.py

--- Word2Vec #1.py
+++ Word2Vec #1.py
@@ -12,97 +12,77 @@ class Word2Vec:
       self.optimizer = tf.optimizers.SGD(learning_rate=0.1)
 
   def train(self, x_train=None, y_train=None):
-    self.W1 = tf.Variable(tf.random.normal([self.vocab_size, self.embedding_dim]))
-    self.b1 = tf.Variable(tf.random.normal([self.embedding_dim])) #bias
-    # W1, 5x7; b1, 5x1; X, 7x1
-    # 5x7 x 7x1 + 5x1 = 5x1
-
-    self.W2 = tf.Variable(tf.random.normal([self.embedding_dim, self.vocab_size]))
-    self.b2 = tf.Variable(tf.random.normal([self.vocab_size]))
-    # W2, 7x5; b2, 7x1; X, 5x1
-    # 7x5 x 5x1 + 7x1 = 7x1
-
+    self.W1 = tf.Variable(tf.random.normal([self.vocab_size, self.embedding_dim])) # 5x7 dimension
+    self.b1 = tf.Variable(tf.random.normal([self.embedding_dim])) # 5x1 dimension
+    self.W2 = tf.Variable(tf.random.normal([self.embedding_dim, self.vocab_size])) # 7x5 dimension
+    self.b2 = tf.Variable(tf.random.normal([self.vocab_size])) # 7x1 dimension
     for _ in range(self.epochs):
       with tf.GradientTape() as t:
-        hidden_layer = tf.add(tf.matmul(x_train,self.W1),self.b1)
-        output_layer = tf.nn.softmax(tf.add( tf.matmul(hidden_layer, self.W2), self.b2))
-      #  cross_entropy_loss = tf.reduce_mean( -tf.math.reduce_sum( y_train * tf.math.log(output_layer), axis=[1] ) )
-        temp1 = tf.math.log(output_layer)
-        temp2 = -tf.math.reduce_sum(y_train *temp1, axis=[1])
-        cross_entropy_loss = tf.reduce_mean(temp2)
+        # there is only one hidden layer in this code
+        hidden_layer = tf.add(tf.matmul(x_train,self.W1),self.b1) # 34x7 * 7x5 + 5 scalars = 34x5
+        # the output layer uses softmax regression to finalize the output
+        output_layer = tf.nn.softmax(tf.add(tf.matmul(hidden_layer, self.W2), self.b2)) # 34x5 * 5x7 + 7 scalars = 34x7
+        # cross entropy is used as the loss function because all the output values are between 0 and 1
+        cross_entropy_loss = tf.reduce_mean(-tf.math.reduce_sum( y_train * tf.math.log(output_layer), axis=[1]))
+      # performing optimization on each variable
       grads = t.gradient(cross_entropy_loss, [self.W1, self.b1, self.W2, self.b2])
       t1 = zip(grads,[self.W1, self.b1, self.W2, self.b2])
       self.optimizer.apply_gradients(t1)
-
       if(_ % 1000 == 0):
         print(cross_entropy_loss)
 
-  def vectorized(self, word_idx):
-    return (self.W1+self.b1)[word_idx]
+  def vectorized(self,index):
+    return (self.W1+self.b1)[index]
 
-corpus_raw = 'He is the king . The king is royal . She is the royal  queen '
-corpus_raw = corpus_raw.lower()
-# raw sentences is a list of sentences.
-raw_sentences = corpus_raw.split('.')
+initalPhrase = 'He is the king . The king is royal . She is the royal  queen '
+rawSentences = initalPhrase.lower().split('.')
+
 sentences = []
-for sentence in raw_sentences:
+for sentence in rawSentences:
     sentences.append(sentence.split())
 
+# create an list that contains every combination of each word with its two closest neighbors on each side
+# The first and last word only have two neighbors each while the second and second last word only have three neighbors each
 data = []
-WINDOW_SIZE = 2
+windowSize = 2
 for sentence in sentences:
     for word_index, word in enumerate(sentence):
+        for neighborWord in sentence[max(word_index - windowSize, 0):min(word_index + windowSize, len(sentence)) + 1]:
+            if neighborWord != word:
+                data.append([word, neighborWord])
 
-        # for nb_word in sentence[max(word_index - WINDOW_SIZE, 0) : min(word_index + WINDOW_SIZE, len(sentence)) + 1] :
-        temp1 = max(word_index - WINDOW_SIZE, 0)
-        temp2 = min(word_index + WINDOW_SIZE, len(sentence)) + 1
-        for nb_word in sentence[temp1:temp2 ]:
-            if nb_word != word:
-                data.append([word, nb_word])
+# creating a list that contains every distinctive word in the initial phrase
+distinctiveWords = []
+for word in initalPhrase.split():
+    if word != '.':
+        distinctiveWords.append(word)
+distinctiveWords = set(distinctiveWords)
+vocab_size = len(distinctiveWords)
 
-words = []
-for word in corpus_raw.split():
-    if word != '.': # because we don't want to treat . as a word
-        words.append(word)
-words = set(words) # so that all duplicate words are removed
+# creating two directories using all indexes and words
 word2int = {}
 int2word = {}
-vocab_size = len(words) # gives the total number of unique words
-for i,word in enumerate(words):
+for i,word in enumerate(distinctiveWords):
     word2int[word] = i
     int2word[i] = word
-
-
-# function to convert numbers to one hot vectors
-def to_one_hot(data_point_index, vocab_size):
-    temp = np.zeros(vocab_size)
-    temp[data_point_index] = 1
-    return temp
+    
+# performing one-hot encoding on one word
+def to_one_hot(index, length):
+    oneHotArray = np.zeros(length)
+    oneHotArray[index] = 1
+    return oneHotArray
 
 x_train = [] # input word
 y_train = [] # output word
-
 for data_word in data:
-    x_train.append( to_one_hot(word2int[ data_word[0] ], vocab_size) )
-    y_train.append( to_one_hot(word2int[ data_word[1] ], vocab_size) )
-# split the training data to the input words and output words
-# use one-hot vectors to represent the words
-
-# convert them to numpy arrays
+    x_train.append(to_one_hot(word2int[data_word[0]], vocab_size))
+    y_train.append(to_one_hot(word2int[data_word[1]], vocab_size))
 x_train = np.asarray(x_train, dtype='float32')
 y_train = np.asarray(y_train, dtype='float32')
-
-# instantiation, same thing as creating an object with parameters
 w2v = Word2Vec(vocab_size=vocab_size, optimizer='adam', epochs=10000)
 w2v.train(x_train, y_train)
 
-
-temp = w2v.vectorized(word2int['queen'])
-print(temp)
-
-
-vectors = w2v.W1 + w2v.b1
-print(vectors)
+print(w2v.W1 + w2v.b1)
 
 def euclidean_dist(vec1, vec2):
     return np.sqrt(np.sum((vec1-vec2)**2))
@@ -121,8 +101,6 @@ print(int2word[find_closest(word2int['king'], vectors)])
 print(int2word[find_closest(word2int['queen'], vectors)])
 print(int2word[find_closest(word2int['royal'], vectors)])
 
-
-
 from sklearn.manifold import TSNE
 from sklearn import preprocessing
 model = TSNE(n_components=2, random_state=0)
@@ -130,7 +108,6 @@ np.set_printoptions(suppress=True)
 vectors = model.fit_transform(vectors)
 normalizer = preprocessing.Normalizer()
 vectors =  normalizer.fit_transform(vectors, 'l2')
-
 
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
@@ -140,6 +117,3 @@ for word in words:
     print(word, vectors[word2int[word]][1])
     ax.annotate(word, (vectors[word2int[word]][0],vectors[word2int[word]][1] ))
 plt.show()
-
-
-print("end")
